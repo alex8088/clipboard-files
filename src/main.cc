e93801a90cecf1a47409bc55527a9172a0840060
谢英杰(YingjieXie)-顺丰科技技术集团 <xieyingjie@sf-express.com>
2023-02-24 18:04:11 +0800

    feat: add nodejs context-aware support

--- src/main.cc
+++ src/main.cc
@@ -7,28 +7,29 @@
 
 namespace clipboard
 {
-	using v8::Context;
 	using v8::Array;
+	using v8::Context;
 	using v8::FunctionCallbackInfo;
 	using v8::Isolate;
 	using v8::Local;
 	using v8::MaybeLocal;
+	using v8::NewStringType;
 	using v8::Object;
 	using v8::String;
-	using v8::NewStringType;
 	using v8::Value;
 
 	void readFiles(const FunctionCallbackInfo<Value> &args)
 	{
 		Isolate *isolate = args.GetIsolate();
-        Local<Array> fileNames = get_file_names(isolate);
-        args.GetReturnValue().Set(fileNames);
+		Local<Array> fileNames = get_file_names(isolate);
+		args.GetReturnValue().Set(fileNames);
 	}
 
-    void writeFiles(const FunctionCallbackInfo<Value> &args)
+	void writeFiles(const FunctionCallbackInfo<Value> &args)
 	{
-		if (args[0]->IsArray()) {
-            Isolate *isolate = args.GetIsolate();
+		if (args[0]->IsArray())
+		{
+			Isolate *isolate = args.GetIsolate();
 			Local<Array> array = Local<Array>::Cast(args[0]);
 			write_file_names(isolate, array);
 		}
@@ -40,5 +41,16 @@ namespace clipboard
 		NODE_SET_METHOD(exports, "writeFiles", writeFiles);
 	}
 
-	NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
+	// NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
+
+	extern "C" NODE_MODULE_EXPORT void
+	NODE_MODULE_INITIALIZER(Local<Object> exports,
+							Local<Value> module,
+							Local<Context> context)
+	{
+		/* Perform addon initialization steps here. */
+		Init(exports);
+	}
+
+	NODE_MODULE_CONTEXT_AWARE(NODE_GYP_MODULE_NAME, NODE_MODULE_INITIALIZER)
 }
