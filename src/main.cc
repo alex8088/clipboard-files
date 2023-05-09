35bc1d560ee36deb1bd03b18570f6dfb0e6b6094
bubuzi <blog.bubuzi@gmail.com>
2023-05-09 15:51:30 +0800

    Change Nan to Napi

--- src/main.cc
+++ src/main.cc
@@ -1,44 +1,27 @@
-﻿#include <node.h>
+﻿#include <napi.h>
 #ifdef __WIN32__
 #include "clip_win.h"
 #elif __APPLE__
 #include "clip_osx.h"
 #endif
 
-namespace clipboard
+Napi::Value get_file_names(const Napi::CallbackInfo &info);
+
+Napi::Value write_file_names(const Napi::CallbackInfo &info);
+
+static Napi::Value readFiles(const Napi::CallbackInfo &info)
 {
-	using v8::Context;
-	using v8::Array;
-	using v8::FunctionCallbackInfo;
-	using v8::Isolate;
-	using v8::Local;
-	using v8::MaybeLocal;
-	using v8::Object;
-	using v8::String;
-	using v8::NewStringType;
-	using v8::Value;
-
-	void readFiles(const FunctionCallbackInfo<Value> &args)
-	{
-		Isolate *isolate = args.GetIsolate();
-        Local<Array> fileNames = get_file_names(isolate);
-        args.GetReturnValue().Set(fileNames);
-	}
-
-    void writeFiles(const FunctionCallbackInfo<Value> &args)
-	{
-		if (args[0]->IsArray()) {
-            Isolate *isolate = args.GetIsolate();
-			Local<Array> array = Local<Array>::Cast(args[0]);
-			write_file_names(isolate, array);
-		}
-	}
-
-	void Init(Local<Object> exports)
-	{
-		NODE_SET_METHOD(exports, "readFiles", readFiles);
-		NODE_SET_METHOD(exports, "writeFiles", writeFiles);
-	}
-
-	NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
+	return get_file_names(info);
 }
+static Napi::Value writeFiles(const Napi::CallbackInfo &info)
+{
+	return write_file_names(info);
+}
+static Napi::Object Init(Napi::Env env, Napi::Object exports)
+{
+	exports["readFiles"] = Napi::Function::New(env, readFiles);
+	exports["writeFiles"] = Napi::Function::New(env, writeFiles);
+	return exports;
+}
+
+NODE_API_MODULE(NODE_GYP_MODULE_NAME, Init)
